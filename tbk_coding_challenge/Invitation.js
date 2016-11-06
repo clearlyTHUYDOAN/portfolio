bd7165968b970a94c050b18ced4ca6e7eefbe1e8
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2016-11-05 23:01:23 -0400

    progress: layout + most css

---
+++ tbk_coding_challenge/Invitation.js
@@ -0,0 +1,12 @@
+/* When user has been on the site for 3 seconds, deploy popover. */
+setTimeout(function() {
+    console.log("setTimeOut works for this challenge.");
+    /* Test popover. */
+    // $("#demo01").animatedModal();
+}, 2000);
+
+
+/* If user closes the popover, do not deploy on subsequent reloads/pageviews. */
+$(".close-button").click(function() {
+    console.log("Close button was clicked.")
+});
