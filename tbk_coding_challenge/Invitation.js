8bec3c4197f89db030a83d3c334286aab6f8865a
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2016-11-06 21:21:11 -0500

    save popover progress

--- tbk_coding_challenge/Invitation.js
+++ tbk_coding_challenge/Invitation.js
@@ -1,14 +1,17 @@
-/* When user has been on the site for 3 seconds, deploy popover. */
-setTimeout(function() {
-    console.log("setTimeOut works for this challenge.");
-    /* Test popover. */
-    // $("#demo01").animatedModal();
-}, 2000);
+$(document).ready(function() {
+     
+      // When user has been on the site for 3 seconds, deploy popover. //
+      // This jQuery plugin is accessible by default. //
+    //   setTimeout(function() {
+    //       $('#my_popup').popup('show'); 
+    //       console.log("setTimeOut works.");
+    //   }, 3000);
 
+      // Source: http://dev.vast.com/jquery-popup-overlay/ //
 
-/* If user closes the popover, do not deploy on subsequent reloads/pageviews. */
-$(".close-button").click(function() {
-    console.log("Close button was clicked.")
+    /* If user closes the popover, do not deploy on subsequent reloads/pageviews. */
+    $(".close-button").click(function() {
+        $('#my_popup').popup('hide');
+    });
+
 });
-
-/* Accessility */
