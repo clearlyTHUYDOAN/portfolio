439f8b962a9f3dbab6a167d1cc8d3a0db1df455c
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2016-11-07 13:04:02 -0500

    update reponsiveness

--- tbk_coding_challenge/Invitation.js
+++ tbk_coding_challenge/Invitation.js
@@ -2,10 +2,15 @@ $(document).ready(function() {
      
       // When user has been on the site for 3 seconds, deploy popover. //
       // This jQuery plugin is accessible by default. //
-    //   setTimeout(function() {
-    //       $('#my_popup').popup('show'); 
-    //       console.log("setTimeOut works.");
-    //   }, 3000);
+      
+      // Initialize plugin.
+      $('#my_popup').popup(); 
+      // Prevent flashing of popup on page load.
+      $('#my_popup').popup('hide');
+      $('#my_popup').removeClass('hidden');
+      setTimeout(function() {
+          $('#my_popup').popup('show'); 
+      }, 0);
 
       // Source: http://dev.vast.com/jquery-popup-overlay/ //
 
